0d52dfa7489e022c46de88d4f1feca0ee1121220
Daniel Duan <daniel4duan@gmail.com>
2021-05-04 22:29:05 -0400

    summary

--- Location.java
+++ Location.java
@@ -3,11 +3,13 @@ public class Location {
     private double longitude;
     private double latitude;
     private String address;
+    private String name;
 
-    public Location(double longitude, double latitude, String address) {
+    public Location(double longitude, double latitude, String address, String name) {
         this.longitude = longitude;
         this.latitude = latitude;
         this.address = address;
+        this.name = name;
     }
 
     public double getLongitude() {
@@ -20,5 +22,9 @@ public class Location {
 
     public String getAddress() {
         return this.address;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
